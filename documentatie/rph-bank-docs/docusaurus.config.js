32a15c7cd3036cc87a39cfc2a4e21e7407f659b0
Aya Boutaarourte <aya.boutaarourte@student.odisee.be>
2026-04-28 10:48:56 +0200

    Fix routes

--- documentatie/rph-bank-docs/docusaurus.config.js
+++ documentatie/rph-bank-docs/docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   },
 
   url: 'https://usernaya.github.io',
-  baseUrl: '/IW2-Team13-BankingProject-2526/',
+  baseUrl: '/',
 
   organizationName: 'usernaya',
   projectName: 'IW2-Team13-BankingProject-2526',
@@ -76,17 +76,17 @@ const config = {
           {
             title: 'Documentatie',
             items: [
-              { label: 'Introductie', to: '/intro' },
-              { label: 'Architectuur', to: '/architectuur' },
-              { label: 'Payment Flow', to: '/po-flow' },
+              { label: 'Introductie', to: 'intro' },
+              { label: 'Architectuur', to: 'architectuur' },
+              { label: 'Payment Flow', to: 'po-flow' },
             ],
           },
           {
             title: 'API',
             items: [
-              { label: 'Accounts', to: '/accounts/list' },
-              { label: 'Payments', to: '/payments/new' },
-              { label: 'Errorcodes', to: '/errorcodes' },
+              { label: 'Accounts', to: 'accounts/list' },
+              { label: 'Payments', to: 'payments/new' },
+              { label: 'Errorcodes', to: 'errorcodes' },
             ],
           },
           {
