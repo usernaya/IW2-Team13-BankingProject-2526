56f2b8925237aea53afd0ed8d9d3cdac97e7fa5b
Aya Boutaarourte <aya.boutaarourte@student.odisee.be>
2026-04-28 10:13:12 +0200

    Revert "docs: add static build of Docusaurus documentation"
    This reverts commit 80a754a12fc06a52545678be1befba5a277d4d3d.

--- documentatie/rph-bank-docs/build/assets/js/aba21aa0.e9606b18.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkrph_bank_docs=self.webpackChunkrph_bank_docs||[]).push([["747"],{7093(s){s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
